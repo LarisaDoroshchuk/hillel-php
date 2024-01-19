eb0e5fc11921d969eef275be10392700f6698505
Larisa Doroshchuk <larisa.doroshchuk@gmail.com>
2024-01-19 13:37:35 +0100

    hillel_hw_10

--- hillel_hw/php_hw_10/command.php
+++ hillel_hw/php_hw_10/command.php
@@ -1,37 +1,21 @@
 <?php
 declare(strict_types=1);
 
-function fibonacci(int $n): int
-{
-    $fib = [0, 1];
-
-    for ($i = 2; $i <= $n; $i++) {
-        $fib[$i] = $fib[$i - 1] + $fib[$i - 2];
-    }
-
-    return $fib[$n];
-}
-
-//echo(fibonacci(10));
-
-
 function generatorFibonacciNumber(int $maxNumber): Generator
 {
+    $fib = [0, 1];
     $i = 0;
-    do {
-        $fibonacciNumber = fibonacci($i);
-        if ($fibonacciNumber >= $maxNumber) {
-            return;
-        }
-        yield $fibonacciNumber;
+
+    while ($fib[$i] < $maxNumber) {
+        yield $fib[$i];
+        $fib[] = $fib[$i] + $fib[$i + 1];
         $i++;
-    } while (true);
+    }
 }
 
 $generatorFibonacci = generatorFibonacciNumber(20000);
 
 foreach ($generatorFibonacci as $value) {
     echo $value . PHP_EOL;
-};
-
+}
 
